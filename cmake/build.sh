3a646e4acbcc8cb8f469cb7120b56269628895bc
John Palmer <jpal8929@physics.usyd.edu.au>
2015-07-24 11:41:52 +1000

    update cmake to 3.3.0

--- cmake/build.sh
+++ cmake/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-CC=cc CXX=c++ ./configure --prefix=$PREFIX --parallel=${CPU_COUNT}
+CC=cc CXX=c++ ./bootstrap --system-libs --no-system-jsoncpp --prefix=$PREFIX --parallel=${CPU_COUNT} -- -DCMAKE_USE_OPENSSL=ON
 make -j${CPU_COUNT}
 rm $SRC_DIR/Modules/CPack.OSXScriptLauncher.in
 make install
